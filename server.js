2d0a9c5109fc45ec61c448bc1890781d956bdcca
priyamtha <priyamtha1509@gmail.com>
2025-02-10 15:07:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+require ('dotenv').config;
+const cors = require('cors')
+
+app.get("/",(req,res)=>{
+    res.status(200).send("done")
+
+})
+app.post("/",(req,res)=>{
+    const main{username}=req.body
+    if(!name){
+        return res.status(400).json{"username cannot be empty"}
+    }
+app.post("/"(req,res))=>{
+    const main{email}=req.body
+    if(!email){
+        return res.status(400).json{"email cannot be empty"}
+    }
+app.post("/"(req,res))=>{
+    const main{password}=req.body
+    if(!password){
+        return res.status(400).json{"password cannot be empty"}
+    }
+app.post("/"(req,res))=>{
+    const main{date of birth}=req.body
+    if(!date of birth){
+        return res.status(400).json{"date of birth cannot be empty"}
+    }
+}
+}
+}
+app.listen(process.env.port,)
+console.log("done")
+})
+    
+
